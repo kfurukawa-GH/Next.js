edffd30732798bfe5da16e14c63352963385839e
kfurukawa-GH <104618454+kfurukawa-GH@users.noreply.github.com>
2023-12-18 21:36:56 +0900

    InputコンポーネントにChangeEventで入力された値をuseStateで保持する処理を追加

--- apps/new_food_doc_app/app/_components/elements/Input/Input.tsx
+++ apps/new_food_doc_app/app/_components/elements/Input/Input.tsx
@@ -1,8 +1,22 @@
+"use client";
+import { useState } from "react";
 import { InputVariants } from "./TailwindVariants";
 import { InputAttributes } from "@/app/_types/tagAttributes";
 
 const Input = (props: InputAttributes & { children?: React.ReactNode }) => {
-  return <input {...props} className={InputVariants()} />;
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <input
+      {...props}
+      value={inputValue}
+      onChange={handleInputChange}
+      className={InputVariants()}
+    />
+  );
 };
 
 export default Input;
