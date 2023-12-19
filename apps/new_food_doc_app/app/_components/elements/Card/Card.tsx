eeeed016ad62809c9ec577b200cd7db3afef1b13
kfurukawa-GH <104618454+kfurukawa-GH@users.noreply.github.com>
2023-12-19 17:43:44 +0900

    仮カード作成

--- apps/new_food_doc_app/app/_components/elements/Card/Card.tsx
+++ apps/new_food_doc_app/app/_components/elements/Card/Card.tsx
@@ -11,19 +11,59 @@ type Prop = {
 
 const Card = ({ imageAsset, items, items2 }: Prop) => {
   return (
-    <div className={CardClasses()}>
-      <div className="flex-none h-full w-1/6 relative p-3">
-        <Image src={imageAsset} alt="食品画像" className="object-cover h-full w-full rounded-xl" priority />
+    <div className="bg-white rounded-lg m-3 p-4">
+      <div className="px-4 sm:px-0">
+        <h3 className="text-base font-semibold leading-7 text-gray-900">
+          基本情報
+        </h3>
       </div>
-      <div className="flex-1">
-        {items.map((item) => (
-          <DescriptionList term={item.term} description={item.description} />
-        ))}
-      </div>
-      <div className="flex-1">
-        {items2.map((item) => (
-          <DescriptionList term={item.term} description={item.description} />
-        ))}
+      <div className="mt-6 border-t border-gray-100">
+        <dl className="divide-y divide-gray-100">
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              正式商品名
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              テスト
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              JICFS分類
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              テスト
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              荷姿規格
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              発売日
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              メーカー名
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              $120,000
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              メーカー商品ID
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
+              incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
+              consequat sint. Sit id mollit nulla mollit nostrud in ea officia
+              proident. Irure nostrud pariatur mollit ad adipisicing
+              reprehenderit deserunt qui eu.
+            </dd>
+          </div>
+        </dl>
       </div>
     </div>
   );
