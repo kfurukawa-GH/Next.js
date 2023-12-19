9fe482ff074609c628f180b5b31ed30ce1b63ca6
kfurukawa-GH <104618454+kfurukawa-GH@users.noreply.github.com>
2023-12-19 15:19:37 +0900

    tbodyタグを削除

--- apps/new_food_doc_app/app/_components/elements/Table/TableBody.tsx
+++ apps/new_food_doc_app/app/_components/elements/Table/TableBody.tsx
@@ -2,15 +2,13 @@ import { TableBodyProps } from "@/app/_types/props";
 
 const TableBody = (props: { bodies: TableBodyProps[] }) => {
   return (
-    <tbody>
-      <tr className="odd:bg-gray-900 even:bg-gray-800">
-        {props.bodies.map((body) => (
-          <td key={body.value} scope="col" className="px-6 py-3">
-            {body.value}
-          </td>
-        ))}
-      </tr>
-    </tbody>
+    <tr className="odd:bg-gray-900 even:bg-gray-800">
+      {props.bodies.map((body) => (
+        <td key={body.value} scope="col" className="px-6 py-3">
+          {body.value}
+        </td>
+      ))}
+    </tr>
   );
 };
 
